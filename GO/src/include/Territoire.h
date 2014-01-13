65e1c6d46a200a1dfe606ff25fe51313c19a698b
Goutye <gaut.b@sfr.fr>
2014-01-13 18:03:28 +0100

    Territoire partiellement fait. J'ai bcp de modif à faire par la suite : Plateau, chaine, pion, etc... On va en discuter avant. J'assume ma connerie x)

--- GO/src/include/Territoire.h
+++ GO/src/include/Territoire.h
@@ -7,9 +7,17 @@
 #define TERRITOIRE_H_
 
 #include "Chaine.h"
+#include "Plateau.h"
+#include "Position.h"
 
 typedef Chaine Territoire;
 
-
+/**
+ * Determine le territoire de la case origine
+ * @param plateau
+ * @param pos
+ * @return territoire : Chaine des cases du territoires
+ */
+Territoire determineTerritoire(Plateau plateau,Position origine);
 
 #endif
