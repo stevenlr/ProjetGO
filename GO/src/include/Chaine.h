a9a42a2794c9a1a06a993a35fcb07ead0f15641e
Steven Le Rouzic <steven.lerouzic@gmail.com>
2014-01-14 15:01:56 +0100

    Il faut pas enlever les espaces Eclipse !

--- GO/src/include/Chaine.h
+++ GO/src/include/Chaine.h
@@ -12,77 +12,77 @@
 typedef struct Chaine* Chaine;
 
 /**
-* Crée une chaine.
-*
-* @return Chaine créée.
-*/
+ * Crï¿½e une chaine.
+ *
+ * @return Chaine crï¿½ï¿½e.
+ */
 Chaine Chaine_creer();
 
 /**
-* Détruit une chaine.
-*
-* @param chaine
-*/
+ * Dï¿½truit une chaine.
+ *
+ * @param chaine
+ */
 void Chaine_detruire(Chaine chaine);
 
 /**
-* Désalloue toutes les positions stockées.
-*
-* @param chaine
-*/
+ * Dï¿½salloue toutes les positions stockï¿½es.
+ *
+ * @param chaine
+ */
 void Chaine_vider(Chaine chaine);
 
 /**
-* Verifie si un pion appartient à  la chaine
-* @param chaine
-* @param pion
-* @return 1 si le pion est dans la chaine, sinon 0
-*/
+ * Verifie si un pion appartient ï¿½  la chaine
+ * @param chaine
+ * @param pion
+ * @return 1 si le pion est dans la chaine, sinon 0
+ */
 int Chaine_appartient(Chaine chaine, Position position);
 
 /**
-* Vérifie si une chaine est vide.
-*
-* @param chaine
-* @return 0 si non vide, 1 si vide.
-*/
+ * Vï¿½rifie si une chaine est vide.
+ *
+ * @param chaine
+ * @return 0 si non vide, 1 si vide.
+ */
 int Chaine_estVide(Chaine chaine);
 
 /**
-* Retourne l'élément courant de la chaine.
-* @param chaine
-* @return Position.
-*/
+ * Retourne l'ï¿½lï¿½ment courant de la chaine.
+ * @param chaine
+ * @return Position.
+ */
 Position Chaine_courant(Chaine chaine);
 
 /**
-* Se place à l'élément suivant de la chaine.
-* @param chaine
-* @return 1 si on a pu s'y placer, 0 si on était à la fin.
-*/
+ * Se place ï¿½ l'ï¿½lï¿½ment suivant de la chaine.
+ * @param chaine
+ * @return 1 si on a pu s'y placer, 0 si on ï¿½tait ï¿½ la fin.
+ */
 int Chaine_suivant(Chaine chaine);
 
 /**
-* Se place à la tête de la chaine.
-*
-* @param chaine
-*/
+ * Se place ï¿½ la tï¿½te de la chaine.
+ *
+ * @param chaine
+ */
 void Chaine_tete(Chaine chaine);
 
 /**
-* Insère une position dans une chaine.
-*
-* @param chaine
-* @param position
-*/
+ * Insï¿½re une position dans une chaine.
+ *
+ * @param chaine
+ * @param position
+ */
 void Chaine_inserer(Chaine chaine, Position position);
 
 /**
-* Inserer la Couleur couleur à  la chaine;
-*
-* @param chaine
-* @param couleur
-*/
+ * Inserer la Couleur couleur ï¿½  la chaine;
+ *
+ * @param chaine
+ * @param couleur
+ */
 void Chaine_setCouleur(Chaine chaine, Couleur couleur);
 
 /**
