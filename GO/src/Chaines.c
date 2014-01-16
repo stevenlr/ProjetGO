ecfb7fb69ba62eed1e43e1b8e83c82a85af4a65d
Goutye <gaut.b@sfr.fr>
2014-01-16 14:53:47 +0100

    Et j'avais oublié de poster Chaines.c :)

---
+++ GO/src/Chaines.c
@@ -0,0 +1,24 @@
+#include "include/Chaines.h"
+
+int Chaines_positionAppartient(Chaines chaines, Position position)
+{
+	Chaine chaine;
+	int appartientChaine = 0;
+
+	if(Liste_estVide(chaines))
+		return 0;
+
+	Liste_tete(chaines);
+
+	do
+	{
+		chaine = Liste_courant(chaines);
+		if(Chaine_appartient(chaine, position))
+		{
+			appartientChaine = 1;
+		}
+	}while(Liste_suivant(chaines));
+
+	return appartientChaine;
+}
+
